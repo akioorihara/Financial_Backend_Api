d7c43f0ef7bbca01001e4414223be2ed0fd08293
Akio O <akio.orihara@outlook.com>
2026-09-24 21:14:35 -0500

    documenting and learning ef core

--- FinancialBackendApi/FinancialBackendApi/Contexts/FinancialDbContext.cs
+++ FinancialBackendApi/FinancialBackendApi/Contexts/FinancialDbContext.cs
@@ -5,7 +5,12 @@ namespace FinancialBackendApi.Contexts
 {
     public class FinancialDbContext : DbContext
     {
+        // DbSets are how EF Core knows which entities to include to the model and how to map them to database tables.
         public DbSet<FileHeader> fileHeaders { get; set; }
         public DbSet<FileDetail> fileDetails { get; set; }
+
+
+
     }
+
 }
